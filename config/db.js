4035da277e6b22b81dc8d7b3b9587f0d8fabfd39
youarestillugly <justlittletoxic@gmail.com>
2025-05-28 09:18:00 +0600

    UserAuth 3

--- config/db.js
+++ config/db.js
@@ -7,7 +7,10 @@ const db = pgp({
   port: 5432,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS
+  password: process.env.DB_PASS,
+  ssl:{
+    rejectUnauthorized:false
+  }
 });
 
 
